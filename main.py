0f360e4ff69169fe2b31954dbf34e414dda3bbfa
Petros <petroschar9831@gmail.com>
2021-10-18 00:20:37 +0300

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import paixnidi1 as p1
+import paixnidi2 as p2
+print("Game start...")
+i = 0
+num_pextes = 0
+num_pextes = int(input("Dwse arithmo pextwn : "))
+
+while (num_pextes == 0) or (num_pextes > 3):
+    print("O arithmos twn pextwn prepei na einai 1 h 2!")
+    num_pextes = int(input("Dwse arithmo pextwn : "))
+
+if num_pextes == 1:
+    p1.start
+else:
+    p2.start
+
+    
+
+
